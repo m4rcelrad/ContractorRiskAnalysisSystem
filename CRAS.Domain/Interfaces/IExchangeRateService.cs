28dd334e7da7d6345cd0b916b49079489856aa08
m4rcelrad <iamnotmarcel@gmail.com>
2026-03-11 19:20:25 +0100

    feat: Add NbpExchangeRateService for fetching exchange rates and integrate with DI setup

---
+++ CRAS.Domain/Interfaces/IExchangeRateService.cs
@@ -0,0 +1,9 @@
+﻿namespace CRAS.Domain.Interfaces;
+
+/// <summary>
+///     Defines the contract for a service that provides exchange rates.
+/// </summary>
+public interface IExchangeRateService
+{
+    Task<decimal> GetExchangeRateAsync(string currencyCode, DateTime date);
+}
