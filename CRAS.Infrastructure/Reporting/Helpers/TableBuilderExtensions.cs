aaf0a22010d71e610f10f955c398066d7ef76dd8
m4rcelrad <iamnotmarcel@gmail.com>
2026-03-18 14:24:24 +0100

    fix: update TableBuilderExtensions to not assign multiple child elements to a single-child container

--- CRAS.Infrastructure/Reporting/Helpers/TableBuilderExtensions.cs
+++ CRAS.Infrastructure/Reporting/Helpers/TableBuilderExtensions.cs
@@ -5,52 +5,58 @@ namespace CRAS.Infrastructure.Reporting.Helpers;
 
 internal static class TableBuilderExtensions
 {
-    public static void RenderHeader(this IContainer container, string text, IStyleProvider style, bool alignRight = false)
+    extension(IContainer container)
     {
-        var cell = container
-            .BorderBottom(1)
-            .BorderColor(style.BorderColor)
-            .PaddingBottom(5);
-
-        if (alignRight) cell.AlignRight();
-
-        cell.Text(text).Style(style.LabelStyle);
-    }
-
-    public static void RenderData(this IContainer container, string text, IStyleProvider style, bool alignRight = false)
-    {
-        var cell = container
-            .PaddingVertical(4)
-            .BorderBottom(1)
-            .BorderColor(style.BorderColor);
-
-        if (alignRight) cell.AlignRight();
-
-        cell.Text(text).Style(style.BaseStyle).FontSize(9);
-    }
-
-    public static void RenderColoredData(this IContainer container, string text, IStyleProvider style, string color, bool alignRight = false)
-    {
-        var cell = container
-            .PaddingVertical(4)
-            .BorderBottom(1)
-            .BorderColor(style.BorderColor);
-
-        if (alignRight) cell.AlignRight();
-
-        cell.Text(text)
-            .Style(style.BaseStyle)
-            .FontSize(9)
-            .FontColor(color)
-            .SemiBold();
-    }
-
-    public static void RenderMetricCell(this IContainer container, string label, string value, IStyleProvider style)
-    {
-        container.Padding(5).Column(col =>
+        public void RenderHeader(string text, IStyleProvider style, bool alignRight = false)
         {
-            col.Item().Text(label).Style(style.LabelStyle);
-            col.Item().Text(value).Style(style.ValueStyle);
-        });
+            var cell = container
+                .BorderBottom(1)
+                .BorderColor(style.BorderColor)
+                .PaddingBottom(5);
+
+            if (alignRight)
+                cell = cell.AlignRight();
+
+            cell.Text(text).Style(style.LabelStyle);
+        }
+
+        public void RenderData(string text, IStyleProvider style, bool alignRight = false)
+        {
+            var cell = container
+                .PaddingVertical(4)
+                .BorderBottom(1)
+                .BorderColor(style.BorderColor);
+
+            if (alignRight)
+                cell = cell.AlignRight();
+
+            cell.Text(text).Style(style.BaseStyle).FontSize(9);
+        }
+
+        public void RenderColoredData(string text, IStyleProvider style, string color, bool alignRight = false)
+        {
+            var cell = container
+                .PaddingVertical(4)
+                .BorderBottom(1)
+                .BorderColor(style.BorderColor);
+
+            if (alignRight)
+                cell = cell.AlignRight();
+
+            cell.Text(text)
+                .Style(style.BaseStyle)
+                .FontSize(9)
+                .FontColor(color)
+                .SemiBold();
+        }
+
+        public void RenderMetricCell(string label, string value, IStyleProvider style)
+        {
+            container.Padding(5).Column(col =>
+            {
+                col.Item().Text(label).Style(style.LabelStyle);
+                col.Item().Text(value).Style(style.ValueStyle);
+            });
+        }
     }
 }
